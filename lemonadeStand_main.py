4a8d68b1d94f9f43b3dae3f3e62d4f781d567be3
Sheldon1436 <bh547516@georgetownisd.org>
2026-01-23 10:12:16 -0600

    importing shop file

--- lemonadeStand_main.py
+++ lemonadeStand_main.py
@@ -1,1 +1,27 @@
+import shop_function as shopF
+import tkinter as tk
+import random as ran
 
+root = tk.Tk()
+root.title('Lemonade Stand')                  #screen / ui
+root.geometry('700x500')
+
+money = 100
+ice = 0
+lemons = 0
+sugar = 0
+
+while money > 0:
+    list = shopF.purchasing(money,[ice,lemons,sugar])   #daily purchasing
+
+    money = list[0]
+    ice = list[1][0]
+    lemons = list[1][1]
+    sugar = list[1][2]
+
+    money += ran.randint(0,5)   #replace when we code customers
+
+    print('Money:',money)
+    print('Ice:',ice)
+    print('Lemons:',lemons)         #print inventory
+    print('Sugar:',sugar)
