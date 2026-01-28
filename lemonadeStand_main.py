fc7b76dedb52fe8aa1b6d61188840f519b30f431
Sheldon1436 <bh547516@georgetownisd.org>
2026-01-28 11:15:45 -0600

    recipe changing code

--- lemonadeStand_main.py
+++ lemonadeStand_main.py
@@ -8,13 +8,20 @@ inventory = {"money": 100,
 
 while inventory["money"] > 0:       #game loop, each repitition is a day
     list = shopF.purchasing(inventory["money"],[inventory["ice"],inventory["lemons"],inventory["sugar"]])   #daily purchasing
+    
+    recipe = {"ice" : int(input('How much ice would you like to use per cup?')),
+          "lemons" : int(input('How many lemons would you like to use per cup?')),
+          "sugar" : int(input('How much sugar would you like to use per cup?'))}
 
     inventory["money"] = list[0]
     inventory["ice"] = list[1][0]
     inventory["lemons"] = list[1][1]
     inventory["sugar"] = list[1][2]
 
-    inventory["money"] += ran.randint(0,5)   #replace when we get customer code
+    cupsBought = ran.randint(0,30)   #replace when we get customer code
+    inventory['ice'] -= recipe["ice"]*cupsBought
+    inventory['lemons'] -= recipe["lemons"]*cupsBought
+    inventory['sugar'] -= recipe["sugar"]*cupsBought
 
     print('Money:',inventory["money"])
     print('Ice:',inventory["ice"])
