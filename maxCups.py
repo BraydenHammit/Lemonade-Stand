0b9121ad4b4b0dc08ce2f7c05b4ba6703bfbb356
Sheldon1436 <bh547516@georgetownisd.org>
2026-01-30 11:16:30 -0600

    fixed macx cups calculationb

--- maxCups.py
+++ maxCups.py
@@ -1,2 +1,14 @@
 def maxCupCalculate(recipe,inventory):
-    maxCups = (recipe['lemons'] * inventory['lemons'])+(recipe['ice'] * inventory['ice'])+(recipe['sugar'] * inventory['sugar'])
+    maxCupsL = (inventory['lemons']/recipe['lemons'])
+
+    if recipe['ice'] != 0:
+        maxCupsI = (inventory['ice']/recipe['ice'])
+    else:
+        maxCupsI = 999999
+
+    if recipe['sugar'] != 0:
+        maxCupsC = (inventory['sugar']/recipe['sugar'])
+    else:
+        maxCupsC = 999999
+
+    return min(maxCupsL,maxCupsI,maxCupsC,inventory["cups"])
