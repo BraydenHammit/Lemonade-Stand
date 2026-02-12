6f231571342a9158bb5acc90913e894c894de6ff
elijagh <eh544709@georgetownisd.org>
2026-02-12 10:32:36 -0600

    updated lemonadeStand_main.py

--- lemonadeStand_main.py
+++ lemonadeStand_main.py
@@ -3,8 +3,6 @@ from recipe_function import recipeSelect, costSelect
 from inventoryDisplay import displayInv
 from maxCups import maxCupCalculate
 import random as ran
-
-day = 1
 import math as m
 
 inventory = {"money": 100, # definines the inventory for later use in the total over arching code
@@ -19,12 +17,6 @@ recipe = {"cost": 1, # definines the recipe for use in the branching code
           "sugar" : 0}
 
 while inventory["money"] > 0:       #game loop, each repitition is a day
-    print(f"========================================= DAY {day} =========================================")
-
-    displayInv(inventory,0,0,0,0,False)
-
-    print('==========================================================================================')
-
     inventory = purchasing(inventory)   #daily purchasing
     
     recipe = {"ice" : -1,
@@ -48,8 +40,4 @@ while inventory["money"] > 0:       #game loop, each repitition is a day
     profits = recipe["cost"]*cupsBought   
     recipe["cost"] += profits
 
-    print("==========================================================================================")
-
-    displayInv(inventory,recipe,cupsMade,profits,cupsBought,True)
-
-    day += 1
+    displayInv(inventory,recipe,cupsMade,profits,cupsBought)
