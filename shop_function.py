2ecdf971e7934867634cd883792a9eb3b63e2b3f
elijagh <eh544709@georgetownisd.org>
2026-02-13 10:37:20 -0600

    too expensive try again

--- shop_function.py
+++ shop_function.py
@@ -25,28 +25,28 @@ def purchasing(inventory):
                 inventory["money"] -= .75*purchaseAmount
                 inventory["ice"] += purchaseAmount
             else:
-                print('Too expensive!')
+                print('Too expensive! Try again.')
 
         elif purchaseType == 'l':
             if inventory["money"] - 1*purchaseAmount > 0:
                 inventory["money"] -= 1*purchaseAmount
                 inventory["lemons"] += purchaseAmount
             else:
-                print('Too expensive!')
+                print('Too expensive! Try again.')
 
         elif purchaseType == 's':
             if inventory["money"] - .5*purchaseAmount > 0:
                 inventory["money"] -= .5*purchaseAmount
                 inventory["sugar"] += purchaseAmount
             else:
-                print('Too expensive!')
+                print('Too expensive! Try again.')
 
         elif purchaseType == 'c':
             if inventory["money"] - .25*purchaseAmount > 0:
                 inventory["money"] -= .25*purchaseAmount
                 inventory["cups"] += purchaseAmount
             else:
-                print('Too expensive!')
+                print('Too expensive! Try again.')
         
         elif not valid:
             print('Invalid item!')
