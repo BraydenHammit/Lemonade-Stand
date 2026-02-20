b411b83d53dcb3f2690e2f164a11ad5a0589aa2d
Sheldon1436 <bh547516@georgetownisd.org>
2026-02-19 20:30:31 -0600

    fixed achievement image size

--- achievements/achievements.py
+++ achievements/achievements.py
@@ -15,7 +15,7 @@ def achievementM():
 def achievementY():
     achievement365 = tk.Tk()
     achievement365.title("1 Year of Lemonade Stand")
-    achievement365.geometry('100x100') #replace with photo size
+    achievement365.geometry('1512x2016') #replace with photo size
     image30 = tk.PhotoImage(file='achievement365.png')
     label30 = tk.Label(achievement365, image=image30)
     label30.place(x=0, y=0, relwidth=1, relheight=1)
