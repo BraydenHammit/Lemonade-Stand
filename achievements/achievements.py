1347b9f9a36bcd4c72814c94541f6c997ed67df1
Sheldon1436 <bh547516@georgetownisd.org>
2026-02-19 20:41:12 -0600

    updated achievements

--- achievements/achievements.py
+++ achievements/achievements.py
@@ -3,8 +3,8 @@ import tkinter as tk
 def achievementM():
     achievement30 = tk.Tk()
     achievement30.title("1 Month of Lemonade Stand")
-    achievement30.geometry('1512x2016') #replace with photo size
-    image30 = tk.PhotoImage(file='achievement30.png')
+    achievement30.geometry('864x1152') #replace with photo size
+    image30 = tk.PhotoImage(file='achievements/achievement30.png')
     label30 = tk.Label(achievement30, image=image30)
     label30.place(x=0, y=0, relwidth=1, relheight=1)
 
@@ -15,8 +15,8 @@ def achievementM():
 def achievementY():
     achievement365 = tk.Tk()
     achievement365.title("1 Year of Lemonade Stand")
-    achievement365.geometry('1512x2016') #replace with photo size
-    image30 = tk.PhotoImage(file='achievement365.png')
+    achievement365.geometry('864x1152') #replace with photo size
+    image30 = tk.PhotoImage(file='achievements/achievement365.png')
     label30 = tk.Label(achievement365, image=image30)
     label30.place(x=0, y=0, relwidth=1, relheight=1)
 
