5ed1a241563770c3da98012c2d5629628518fd47
RougeWarrior <gh557773@georgetownisd.org>
2026-02-27 11:15:58 -0600

    started adding 50k achievement

--- achievements/achievements.py
+++ achievements/achievements.py
@@ -21,3 +21,14 @@ def achievementY():
     label30.place(x=0, y=0, relwidth=1, relheight=1)
 
     achievement365.mainloop()
+
+
+def achievementX():
+    achievement50K = tk.Tk()
+    achievement50K.title("1 Year of Lemonade Stand")
+    achievement50K.geometry('216x288') #replace with photo size
+    image30 = tk.PhotoImage(file='achievements/achievement50K.png')
+    label30 = tk.Label(achievement50K, image=image30)
+    label30.place(x=0, y=0, relwidth=1, relheight=1)
+
+    achievement50K.mainloop()
