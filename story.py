43abb369b281e137eaaf5bc472a510064f33e719
Sheldon1436 <bh547516@georgetownisd.org>
2026-03-05 10:22:58 -0600

    changed story pronouns

--- story.py
+++ story.py
@@ -14,7 +14,7 @@ def story(story,ext,name):
             print("You lost it all. You left with a total of fifty thousand dollars in debt.")
             print("In attempt to pay off your debt, you set up a lemonade stand. Don't go bankrupt.")
         elif story == 2:
-            print(f"Your friend {name} made a bet that you couldn't make fifty thousand dollars. \nHe said you should open a lemonade stand, and make fifty thousand.")
+            print(f"Your friend {name} made a bet that you couldn't make fifty thousand dollars. \nThey said you should open a lemonade stand, and make fifty thousand.")
             loopYN('b')
             if ext == 1:
                 print("You accept the bet. From there begins probably the most legendary... \nWait, that's the wrong script. What I meant was:")
